308b2e8532c39e45db962464bb01753dcca0e9e3
CoolMudit <152211503+CoolMudit@users.noreply.github.com>
2023-11-28 01:41:08 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "Cmngt.h"    //include user defined header file 
+
+int main(int argc, char const *argv[])
+{
+
+   interface();   //calling interface() function to start the Pre owned Car Management System 
+   
+  
+    return 0;
+}
